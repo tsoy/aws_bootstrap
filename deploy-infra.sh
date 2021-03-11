7084850a3516e5f614d2c5782791930fd252f36e
Sergey Tsoy <tsoy.sergey@hotmail.com>
2021-03-11 03:23:41 -0500

    Automatic Deployments: Install CodeDeploy Agent on EC2

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,11 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile aws-bootstrap \
   --query "Account" --output text)
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 # Deploy static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
@@ -32,4 +37,9 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner="$GH_OWNER" \
+    GitHubRepo="$GH_REPO" \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken="$GH_ACCESS_TOKEN" \
+    CodePipelineBucket="$CODEPIPELINE_BUCKET"
